3019ed10252198ef56fab8e5f02d06df31f164af
Jason <jasylwong@gmail.com>
2020-06-05 00:01:16 +0100

    display alert message after clicking on two cards

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Memory Game (JavaScript) 🤔
+
+The classic memory card game, built using vanilla JavaScript.
+Play it right now, by clicking [here](https://jasylwong.github.io/memory-game-js/)!
--- app.js
+++ app.js
@@ -19,15 +19,48 @@ document.addEventListener('DOMContentLoaded', () => {
   ]
 
   const grid = document.querySelector('.grid')
+  let cardsChosen = []
+  let cardsChosenId = []
+  let score = 0
+  document.getElementById('result').textContent = score
 
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
       const card = document.createElement('img')
       card.setAttribute('src', './images/blank.png')
       card.setAttribute('id', i)
+      card.addEventListener('click', flipCard)
       grid.appendChild(card)
     }
   }
+
+  // Check for matches
+  function checkForMatch() {
+    const cards = document.querySelectorAll('img')
+    if (cardsChosen[0] === cardsChosen[1]) {
+      alert('Match!')
+      score += 2
+      document.getElementById('result').textContent = score
+    } else {
+      cards[cardsChosenId[0]].setAttribute('src', './images/blank.png')
+      cards[cardsChosenId[1]].setAttribute('src', './images/blank.png')
+      alert('No match! Try again')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+  }
+
+  // Flip your card
+  function flipCard() {
+    let cardId = this.getAttribute('id')
+    this.setAttribute('src', cardArray[cardId].img)
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    if (cardsChosen.length === 2) { 
+      setTimeout(checkForMatch, 500) 
+    } 
+  }
+
 
   createBoard()
 })
