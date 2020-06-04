fbb8b07b19fd7eff8409eb5f35d246bfda193a1b
Jason <jasylwong@gmail.com>
2020-06-04 22:40:31 +0100

    add cardArray

---
+++ app.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const cardArray = [
+    {
+      name: 'laughing',
+      img: './images/laughing.png'
+    },
+    {
+      name: 'laughing',
+      img: './images/laughing.png'
+    },    {
+      name: 'sunglasses',
+      img: './images/sunglasses.png'
+    },    {
+      name: 'sunglasses',
+      img: './images/sunglasses.png'
+    },    {
+      name: 'wink',
+      img: './images/wink.png'
+    },    {
+      name: 'wink',
+      img: './images/wink.png'
+    },
+  ]
+
+  const grid = document.querySelector.apply('.grid')
+})
