dcc6fb887cca38b734f04498939955d9a465f2a3
jasylwong <jasylwong@gmail.com>
2020-09-20 13:34:09 +0100

    pass test for login valid email, invalid pw

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -15,6 +15,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
+  test 'login with valid email but invalid password' do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'invalid'} }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
   test 'login with valid information' do
     get login_path
     post login_path, params: { session: { email: @user.email,
