dee5c4dbf642e78306acf288e78b853f81bb0a07
jasylwong <jasylwong@gmail.com>
2020-09-24 21:27:25 +0100

    add remember method in SessionsHelper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       reset_session
       log_in user
-      p params[:id]
-      p 'sessions: ', session[:user_id]
+      remember user
       redirect_to user
     else 
       flash.now[:danger] = 'Invalid email/password combination'
